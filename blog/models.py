a38269549b8cf140c364f97deff7a6ca410ceb11
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2019-07-23 21:43:44 +0200

    Added post template

--- blog/models.py
+++ blog/models.py
@@ -10,12 +10,13 @@ user = settings.AUTH_USER_MODEL
 class BlogPost(models.Model):
     user = models.ForeignKey(user, default=1, null=True, on_delete=models.SET_NULL)
     title = models.TextField()
-
+    date = models.DateTimeField(blank=True, null=True)
     # slug title of blog post in url
     slug = models.SlugField(unique=True)
     description = models.TextField(blank=True)
 
-    article_types = [('Programming', 'Programming'), ('UX/UI', 'UX/UI'), ('Electronics', 'Electronics'), ('Other', 'Other')]
+    article_types = [('Programming', 'Programming'), ('UX/UI', 'UX/UI'), ('Electronics', 'Electronics'),
+                     ('Other', 'Other')]
     article_type = models.CharField(max_length=20, choices=article_types, default='Programming')
     content = HTMLField()
 
