6f0fa75655049c579638d6f06de8e074b8c8e8f1
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2019-08-22 19:01:58 +0200

    Remove massive footer redundancy, added url field in model

--- blog/models.py
+++ blog/models.py
@@ -32,6 +32,7 @@ class User(AbstractBaseUser):
     username = models.CharField(max_length=255, unique=True)
     first_name = models.CharField(max_length=255, blank=False, null=False)
     last_name = models.CharField(max_length=255, blank=False, null=False)
+    page_url = models.CharField(max_length=255, unique=True)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
     avatar = models.ImageField()
