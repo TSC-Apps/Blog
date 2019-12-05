9ce18368b92e075089005795855c602ff091a4a5
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2019-12-06 00:04:37 +0100

    Changed exported file folder

--- Backend/settings.py
+++ Backend/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = 'static/'
+STATIC_ROOT = '/opt/assets/static/'
 # for templates not included in app
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
